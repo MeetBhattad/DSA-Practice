498e430cf6c320115d7cbd9205cf9fd8da15ed6f
Meet Bhattad <148578990+MeetBhattad@users.noreply.github.com>
2026-07-08 00:21:20 +0530

    Create Remove Nth Node from the end.cpp

---
+++ Linked List/Remove Nth Node from the end.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head == NULL || head->next == NULL) return NULL;
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* slow = dummy;
+        ListNode* fast = dummy;
+
+        for(int i = 0; i<=n; i++){
+            fast = fast->next;
+        }
+
+        while(fast){
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        slow->next = slow->next->next;
+        return dummy->next;
+    }
+};
