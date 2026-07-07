1781962e2ccefffbe7e9b20adcd73499a6bfc8bf
Meet Bhattad <148578990+MeetBhattad@users.noreply.github.com>
2026-07-07 17:57:54 +0530

    Create ZigZag lv order traversal.cpp

---
+++ Binary Trees/ZigZag lv order traversal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root == NULL) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        bool flag = true;
+
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> row(size);
+            for(int i = 0; i<size; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                int ind = flag? i: size-1-i;
+                row[ind] = node->val;
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            flag = !flag;
+            ans.push_back(row);
+        }
+        return ans;
+    }
+};
