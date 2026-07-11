7f1da71fa5f61aec2c52aa55ed0dd0c3b044ec01
Meet Bhattad <148578990+MeetBhattad@users.noreply.github.com>
2026-07-11 23:54:40 +0530

    Create Reverse words in a string.cpp

---
+++ Strings/Reverse words in a string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.size();
+        reverse(s.begin(), s.end());
+        
+        int i = 0;
+        for(int j = 0; j<n;){
+            while(j<n && s[j] == ' ') j++;
+            if(j == n) break;
+            if(i != 0) s[i++] = ' ';
+            int start = i;
+            while(j < n && s[j] != ' '){
+                s[i++] = s[j++];
+            }
+            reverse(s.begin()+ start, s.begin()+i);
+        }
+        s.resize(i);
+        return s;
+    }
+};
